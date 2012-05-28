efd61e2a0ce31a8c0a20d61cf8e461213bcedee1
Hongtao Ren <renh@iiasa.ac.at>
2012-05-28 18:02:43 +0200

    log

--- src/test/java/com/huaxinshengyuan/socknow/test/PublicationRepositoryTest.java
+++ src/test/java/com/huaxinshengyuan/socknow/test/PublicationRepositoryTest.java
@@ -6,8 +6,6 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.huaxinshengyuan.socknow.domain.Publication;
